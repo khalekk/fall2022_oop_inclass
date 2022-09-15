d8eaca56ddb517f37122bbf1ae9d0fd174b4617c
Khadija Khalek <khadijaakhalek@lewisu.edu>
2022-09-15 12:00:58 -0500

    Added two Java Programs, one named DataTypeExample and the other named DataTypeExamplePart2

---
+++ MyPracticeCode/src/DataTimeExample/App.java
@@ -0,0 +1,7 @@
+package DataTimeExample;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello. It is Wednesday.");
+	}
+}
---
+++ MyPracticeCode/src/DataTypeExamplePart2/App2.java
@@ -0,0 +1,7 @@
+package DataTypeExamplePart2;
+
+public class App2 {
+	public static void main(String[] args) {
+		System.out.println("this is a second attempt.");
+	}
+}
